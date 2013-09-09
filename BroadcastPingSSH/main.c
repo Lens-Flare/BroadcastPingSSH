a5c6d26e050d50e2dda79c44f0693e4c1c390745
Ethan Reesor <ethan.reesor@firelizzard.com>
2013-09-09 19:08:17 -0400

    Initial Commit

---
+++ BroadcastPingSSH/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  BroadcastPingSSH
+//
+//  Created by Ethan Reesor on 9/9/13.
+//  Copyright (c) 2013 Lens Flare. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
